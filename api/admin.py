36d67810a716c1b9b541593de6cd5b7a94c8026d
Roman Rykov <pr174@yandex.ru>
2021-01-13 02:45:54 +0500

    Add genre filter for TitleAdmin

--- api/admin.py
+++ api/admin.py
@@ -30,7 +30,7 @@ class GenreAdmin(admin.ModelAdmin):
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
     list_display = ('name', 'year', 'category')
-    list_filter = ('category', )
+    list_filter = ('category', 'genre')
     search_fields = ('name', )
     inlines = [ReviewInline, ]
 
