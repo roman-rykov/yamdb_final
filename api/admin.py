994ab3bb836a26a376a2a7b857c5707df08dbf74
Roman Rykov <pr174@yandex.ru>
2021-01-12 19:18:22 +0500

    Add model admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Comment, Genre, Review, Title
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 0
+    show_change_link = True
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'category')
+    search_fields = ('name', )
+    inlines = [ReviewInline, ]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title_id', 'text', 'score', 'author')
+    inlines = [CommentInline, ]
