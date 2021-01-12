60b2cbb4c851f15a904562b41fe5c82c62eabae3
Roman Rykov <pr174@yandex.ru>
2021-01-13 02:51:14 +0500

    Add model view sets for Review and Comment

--- api/views.py
+++ api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import pagination
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from .models import Review, Title
+from .serializers import CommentSerializer, ReviewSerializer
+
+
+class ReviewModelViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    pagination_class = pagination.PageNumberPagination
+    pagination_class.page_size = 20
+    # permission_classes = [...]
+
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
+
+    def get_queryset(self):
+        title = self.get_title()
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, title_id=self.get_title())
+
+
+class CommentModelViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    pagination_class = pagination.PageNumberPagination
+    pagination_class.page_size = 20
+    # permission_classes = [...]
+
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
+
+    def get_review(self):
+        return get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'),
+            title_id=self.get_title(),
+        )
+
+    def get_queryset(self):
+        review = self.get_review()
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review_id=self.get_review())
