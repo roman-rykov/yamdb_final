1e64b5a7c780ae15db8febf582e65a6d909881b3
MAKSIM18 <githubogatov@yandex.ru>
2021-01-16 00:41:51 +0300

    fix pep8

--- auth_user/urls.py
+++ auth_user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
-from . import views
+from .import views
 
 v1_router = DefaultRouter()
 v1_router.register('users', views.UserViewSet, basename='users')
