4116f4f9bdd7399833ddc4f23e6bc66203fcd67f
MAKSIM18 <githubogatov@yandex.ru>
2021-01-16 17:34:03 +0300

    fix now user can't change role

--- auth_user/serializer.py
+++ auth_user/serializer.py
@@ -2,21 +2,15 @@ from rest_framework import serializers
 
 from .models import User
 
-fields = ('first_name', 'last_name',
-          'username', 'bio', 'email', 'role')
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = fields
+        fields = ('first_name', 'last_name',
+                  'username', 'bio', 'email', 'role')
 
 
-class UserMeSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = fields
-
+class UserMeSerializers(UserSerializer):
     def get_fields(self):
         fields = super(UserMeSerializers, self).get_fields()
         if self.instance and getattr(self.instance, 'role') == 'user':
