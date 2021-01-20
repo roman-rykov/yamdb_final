ad0326b5abd601fa76dbe598f76d02c0061670aa
MAKSIM18 <githubogatov@yandex.ru>
2021-01-21 01:29:02 +0300

    add check is_auth in IsAdminOrReadOnly permission

--- api/permissions.py
+++ api/permissions.py
@@ -19,4 +19,6 @@ class IsModeratorOrReadOnly(BasePermission):
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return request.method in SAFE_METHODS or request.user.is_superuser
+        return (request.method in SAFE_METHODS or
+                request.user.is_authenticated and
+                (request.user.is_admin or request.user.is_superuser))
