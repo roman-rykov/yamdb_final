d5625c2888dea03ff1265f942702c00bb3e4faad
Roman Rykov <pr174@yandex.ru>
2021-04-19 02:13:54 +0500

    Add missing fixtures in 'fill_base' command

--- auth_user/management/commands/fill_base.py
+++ auth_user/management/commands/fill_base.py
@@ -1,7 +1,8 @@
-from django.core.management.base import BaseCommand
 import csv
-from api.models import Category, Title, Genre
-from auth_user.models import User
+
+from django.core.management.base import BaseCommand
+
+from api.models import Category, Comment, Genre, Review, Title, User
 
 
 def fill_base(file_object, object):
@@ -18,12 +19,34 @@ def fill_base(file_object, object):
             Genre.objects.create(
                 id=line['id'], name=line['name'], slug=line['slug'])
         if object == 'users':
-            User.objects.create(id=int(line['id']),
-                                username=line['username'],
-                                email=line['email'],
-                                role=line['role'],
-                                first_name=line['first_name'],
-                                last_name=line['last_name'])
+            User.objects.create(
+                id=int(line['id']),
+                username=line['username'],
+                email=line['email'],
+                role=line['role'],
+                first_name=line['first_name'],
+                last_name=line['last_name'],
+            )
+        if object == 'genre_title':
+            Title.objects.get(id=int(line['title_id'])).genre.add(
+                int(line['genre_id']))
+        if object == 'review':
+            Review.objects.create(
+                id=int(line['id']),
+                title_id=Title.objects.get(id=int(line['title_id'])),
+                text=line['text'],
+                author=User.objects.get(id=int(line['author'])),
+                score=int(line['score']),
+                pub_date=line['pub_date'],
+            )
+        if object == 'comments':
+            Comment.objects.create(
+                id=int(line['id']),
+                review_id=Review.objects.get(id=int(line['review_id'])),
+                text=line['text'],
+                author=User.objects.get(id=int(line['author'])),
+                pub_date=line['pub_date'],
+            )
 
 
 class Command(BaseCommand):
@@ -32,7 +55,10 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        objects = ('users', 'category', 'genre', 'titles',)
+        objects = (
+            'users', 'category', 'genre', 'titles', 'genre_title', 'review',
+            'comments',
+        )
         for object in objects:
             path = f'data/{object}.csv'
 
